ed6a58d10087439f140369bcfc84c2a56cad250a
onaboat <onaboatatsea99@gmail.com>
2024-08-08 23:08:18 +1000

    adding client cli

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import { CanvasInterface, CanvasClient } from '@dscvr-one/canvas-client-sdk';
+
 import Image from "next/image";
 
 export default function Home() {
+  const canvasClient = new CanvasClient();
+  console.log(canvasClient);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
