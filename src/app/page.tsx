4a1d011101644e57bd13599e241ecc2e95f10195
onaboat <onaboatatsea99@gmail.com>
2024-08-08 23:12:01 +1000

    adding client fixing window

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+"use client"
 import { CanvasInterface, CanvasClient } from '@dscvr-one/canvas-client-sdk';
 
 import Image from "next/image";
 
 export default function Home() {
-  const canvasClient = new CanvasClient();
-  console.log(canvasClient);
+
+  if (typeof window !== 'undefined') {
+    const canvasClient = new CanvasClient();
+    console.log(canvasClient);
+  }
+
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
